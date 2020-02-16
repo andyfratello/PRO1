d4f7bb1b91a9837a1882d0f288d00ecf45b4d45d
andreuorensanz <52325081+andreuorensanz@users.noreply.github.com>
2020-02-16 23:13:51 +0100

    P90615.cc
    Maximum of three integer numbers

---
+++ P1/P90615.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;//max3numbers
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a > b) { 
+        if(a > c) cout << a;
+        else cout << c;
+    } else {
+        if(b > c) cout << b;
+        else cout << c;
+    } cout << endl;
+}
