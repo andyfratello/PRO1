1f63290e2e2908c018ac9889e373a853dd56feca
andyfratello <52325081+andyfratello@users.noreply.github.com>
2021-01-20 21:00:51 +0100

    Insertion sort

---
+++ P10/P41412.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertion_sort(vector<double>& v) {
+	for (int i = 0; i < v.size(); ++i) {
+		double x = v[i];
+		int j = i;
+		while (j > 0 and v[j - 1] > x) {
+			v[j] = v[j - 1];
+			--j;
+		}
+		v[j] = x;
+	}
+}
+
+int main() {
+	cout.setf(ios::fixed, ios::floatfield);
+	cout.precision(4);
+	int n;
+	while (cin >> n) {
+		vector<double> v(n);
+		for (int i = 0; i < n; ++i) cin >> v[i];
+		insertion_sort(v);
+		for (int i = 0; i < n; ++i) cout << ' ' << v[i];
+		cout << endl;
+	}
+}
