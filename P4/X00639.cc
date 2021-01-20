cf78f2627ed1a87de76cf957f70aab92b380119f
andyfratello <52325081+andyfratello@users.noreply.github.com>
2021-01-20 13:37:19 +0100

    Comparing fractions

---
+++ P4/X00639.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+bool c_frac(int n1, int d1, int n2, int d2) {
+	if (double(n1)/d1 < double(n2)/d2) return true;
+	return false;
+}
+
+int main() {
+	int n1, d1, n2, d2;
+	while (cin >> n1 >> d1 >> n2 >> d2) {
+		if (c_frac(n1, d1, n2, d2)) cout << "yes";
+		else cout << "no";
+		cout << endl;
+	}
+}
