d395f8998d2dfe6a7a1c6926fd5ba0400d8644df
andyfratello <52325081+andyfratello@users.noreply.github.com>
2021-01-20 13:34:46 +0100

    Time decomposition (2)

---
+++ P4/P22654.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void decompose(int n, int& h, int& m, int& s) {
+	h = n/3600;
+	m = (n%3600)/60;
+	s = n%60;
+}
+
+int main() {
+	int n, h, m, s;
+	while (cin >> n) {
+		decompose(n, h, m, s);
+		cout << h << ' ' << m << ' ' << s << endl; 
+	}
+}
